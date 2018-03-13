fe190711c7e272805732706531c623ed79b64507
Henrik Sjööh <henriiik@gmail.com>
2018-03-13 21:30:55 +0100

    add stuff

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import "./App.css";
 interface Props {}
 
 interface State {
-	items: string[];
+	items: TodoItem[];
 	value: string;
+	showArchive: boolean;
 }
 
 class App extends React.Component<Props, State> {
 	state: State = {
 		items: [],
 		value: "",
+		showArchive: false,
 	};
 
 	handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,24 +24,42 @@ class App extends React.Component<Props, State> {
 		event.preventDefault();
 		this.setState(prevState => {
 			if (prevState.value === "") {
-				return undefined;
+				return null;
 			}
 			return {
-				items: prevState.items.concat(prevState.value),
+				items: prevState.items.concat({ value: prevState.value, status: "WIP" }),
 				value: "",
 			};
 		});
 	};
 
-	removeItem = (index: number) => {
+	removeItem = (index: number, status?: TodoStatus) => {
+		const newItem = Object.assign({}, this.state.items[index], { status });
+		const items = this.state.items.slice();
+		items.splice(index, 1, newItem);
+		this.setState({ items });
+	};
+
+	archiveDone = () => {
 		this.setState(prevState => {
-			const items = prevState.items.slice();
-			items.splice(index, 1);
+			const items = prevState.items.map(
+				item => (item.status === "done" ? Object.assign({}, item, { status: "archived" }) : item)
+			);
 			return { items };
 		});
 	};
 
+	toggleArchive = () => {
+		this.setState(prevState => {
+			return { showArchive: !prevState.showArchive };
+		});
+	};
+
 	render() {
+		let items = this.state.items;
+		if (!this.state.showArchive) {
+			items = this.state.items.filter(item => item.status !== "archived");
+		}
 		return (
 			<div className="app">
 				<div className="app-content">
@@ -60,16 +80,34 @@ class App extends React.Component<Props, State> {
 						</button>
 					</form>
 					<h2>Todos</h2>
-					<TodoList items={this.state.items} removeItem={this.removeItem} />
+					<TodoList items={items} updateStatus={this.removeItem} />
+					<button className="button" onClick={this.archiveDone}>
+						Archive completed
+					</button>
+					<button className="button" onClick={this.toggleArchive}>
+						{this.state.showArchive ? "Hide" : "Show"} archived
+					</button>
 				</div>
 			</div>
 		);
 	}
 }
 
+type TodoStatus = "WIP" | "done" | "archived";
+const nextStatus: { [K in TodoStatus]: TodoStatus } = {
+	WIP: "done",
+	done: "WIP",
+	archived: "WIP",
+};
+
+interface TodoItem {
+	value: string;
+	status: TodoStatus;
+}
+
 interface TodoListProps {
-	items: string[];
-	removeItem: (index: number) => void;
+	items: TodoItem[];
+	updateStatus: (index: number, status: TodoStatus) => void;
 }
 
 function TodoList(props: TodoListProps) {
@@ -78,19 +116,17 @@ function TodoList(props: TodoListProps) {
 	}
 	return (
 		<ul className="todo-list">
-			{props.items.map((item, index) => {
-				return (
-					<li className="todo-list-item" key={index}>
-						{item}
-						<button
-							className="button todo-list-remove-button"
-							onClick={() => props.removeItem(index)}
-						>
-							Remove
-						</button>
-					</li>
-				);
-			})}
+			{props.items.map((item, index) => (
+				<li className="todo-list-item" key={index}>
+					<span className={item.status}>{item.value}</span>
+					<button
+						className="button todo-list-remove-button"
+						onClick={() => props.updateStatus(index, nextStatus[item.status])}
+					>
+						{nextStatus[item.status]}
+					</button>
+				</li>
+			))}
 		</ul>
 	);
 }
